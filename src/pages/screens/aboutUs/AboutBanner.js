ec9875ac6235e700b92a399178339779ac30eeb3
Suman-89 <suman.mukherjee01@gmail.com>
2023-02-07 00:43:50 +0530

    Aboutus creation

---
+++ src/pages/screens/aboutUs/AboutBanner.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const AboutBanner = () => {
+  return (
+    <div class="page-heading header-text">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h1>About Us</h1>
+          <span>We have over 20 years of experience</span>
+        </div>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default AboutBanner
