12861d0a3c3babc41232352b315713fed646eb77
Suman-89 <suman.mukherjee01@gmail.com>
2023-02-09 00:15:27 +0530

    About us completed and working in Our services

---
+++ src/pages/screens/OurService/OurService.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../../includes/Header'
+import OurServiceBanner from './OurServiceBanner'
+import FooterPart from '../../includes/Footer'
+
+const OurService = () => {
+  return (
+<>
+<Header />
+<OurServiceBanner />
+<FooterPart />
+</>
+  )
+}
+
+export default OurService
--- src/pages/screens/aboutUs/AboutUs.js
+++ src/pages/screens/aboutUs/AboutUs.js
@@ -2,6 +2,10 @@ import React from 'react'
 import Header from '../../includes/Header'
 import AboutBanner from './AboutBanner'
 import AboutOurCompany from './AboutOurCompany'
+import OurSolutions from './OurSolutions'
+import OurTeamMembers from './OurTeamMembers'
+import WhatTheySay from './WhatTheySay'
+import FooterPart from '../../includes/Footer'
 
 const AboutUs = () => {
   return (
@@ -9,6 +13,10 @@ const AboutUs = () => {
      <Header />
      <AboutBanner />
      <AboutOurCompany />
+     <OurTeamMembers />
+     <OurSolutions />
+     <WhatTheySay />
+     <FooterPart />
     </>
   )
 }
