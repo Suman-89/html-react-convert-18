63a772dbe41462ea6d27aa2277fe4ef1b49cdaad
Suman-89 <suman.mukherjee01@gmail.com>
2023-02-09 00:30:03 +0530

    Our services page is under construction

--- src/pages/screens/OurService/OurService.js
+++ src/pages/screens/OurService/OurService.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Header from '../../includes/Header'
 import OurServiceBanner from './OurServiceBanner'
 import FooterPart from '../../includes/Footer'
+import MarketAnalysis from './MarketAnalysis'
 
 const OurService = () => {
   return (
 <>
 <Header />
 <OurServiceBanner />
+<MarketAnalysis />
 <FooterPart />
 </>
   )
